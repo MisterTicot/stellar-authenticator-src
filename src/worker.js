3070f5ed0fedbd0d77bf6c211996b5f142f631a4
MisterTicot <mister.ticot@cosmic.plus>
2019-02-20 11:48:39 +0400

    Tweak vendors configuration & improve app caching

--- src/worker.js
+++ src/worker.js
@@ -5,12 +5,26 @@ const pkg = require("../package.json")
 
 new ServiceWorker(pkg.name, pkg.version, "verbose")
   .fromCache([
+    // Application
     "/",
     "cosmic-lib.css",
     "index.css",
     "index.html",
     "index.js",
-    "stellar-sdk.js"
+    "stellar-sdk.js",
+
+    // Vendor configuration
+    "browserconfig.xml",
+    "manifest.json",
+
+    // Icons
+    "icons/16x16.png",
+    "icons/32x32.png",
+    "icons/192x192.png",
+    "icons/512x512.png",
+    "icons/apple-touch.png",
+    "icons/mstile.png",
+    "icons/safari.svg"
   ])
   .precache()
   .register()
